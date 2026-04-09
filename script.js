d2995cd66506145654a152b35d0812d6a2efe271
Juanel2804 <juanelpidio2004@gmail.com>
2026-04-09 13:57:50 -0600

    respponsive

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+const toggle = document.getElementById("menuToggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
